feb7c4ed26bb36fb1e78fd5ac1295c4ec69e8150
anacirqueira <cirqueiraana6@gmail.com>
2023-08-11 23:17:55 -0300

    Criando o estado da aplicação

--- src/FocusTimer/index.js
+++ src/FocusTimer/index.js
@@ -1,3 +1,8 @@
-export function start(minuts, seconds) {
-  
+import state from './state.js'
+
+export function start(minutes, seconds) {
+  state.minutes = minutes
+  state.seconds = seconds
+
+
 }
