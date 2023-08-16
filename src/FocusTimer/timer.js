7b22b766fb92c2d6bf8d9ff5457fc59e5c5ca1f2
anacirqueira <cirqueiraana6@gmail.com>
2023-08-16 18:30:39 -0300

    Atualizando os dados do contador

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,5 +1,13 @@
-import './state.js'
+import state from './state.js'
 import * as el from './elements.js'
+
+export function countdown() {
+  if(!state.isRunning) {
+    return
+  }
+
+  setTimeout(() => countdown(), 1000)
+}
 
 export function updateDisplay (minutes, seconds) {
   minutes = minutes ?? state.minutes /* nullish coaseling operator */
